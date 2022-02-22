ee12679418fc91c53ee41382c9411f6bee65aa85
Jakub Klik <Jakub.klik6@gmail.com>
2022-02-22 10:37:07 +0100

    Update Zad 1.cs

--- Lab1/Zad 1.cs
+++ Lab1/Zad 1.cs
@@ -8,9 +8,9 @@ namespace ConsoleApp1
         {
             double n;
             Console.Write("Podaj numer wyrazu ciągu n: ");  
-            n = Convert.ToDouble(Console.ReadLine());       // prosumy użytkownika o podanie numeru wyrazu ciągu n
-            Console.WriteLine("Wartość ciągu a(n)=3n-1 dla n = {0}, wynosi {1}",n,3*n-1);       //obliczamy wartość ciągu dla podanego n, wypisujemy na tablice
-            Console.ReadKey(true);      //pauza
+            n = Convert.ToDouble(Console.ReadLine());       
+            Console.WriteLine("Wartość ciągu a(n)=3n-1 dla n = {0}, wynosi {1}",n,3*n-1);      
+            Console.ReadKey(true);      
 
 
 
