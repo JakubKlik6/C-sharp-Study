e5f1be0991e85b8ae1253605bbb8f920c9b12520
Jakub Klik <Jakub.klik6@gmail.com>
2022-02-22 10:48:18 +0100

    Update Zad 3.cs

--- Lab1/Zad 3.cs
+++ Lab1/Zad 3.cs
@@ -1,3 +1,6 @@
+//Zadanie 3
+//Napisać program wczytujący promień koła i wypisujący na ekranie jego pole i obwód.
+
 using System;
 
 namespace ConsoleApp1
@@ -8,10 +11,10 @@ namespace ConsoleApp1
         {
             double r;
             Console.Write("Podaj wartośc r= ");
-            r = Convert.ToDouble(Console.ReadLine());      
-            Console.WriteLine("Pole koła o promieniu {0} wynosi {1}",r, Math.PI*r*r);     
-            Console.WriteLine("Obwód koła o promieniu {0} wynosi {1}", r, 2*Math.PI*r);    
-            Console.ReadKey(true);      
+            r = Convert.ToDouble(Console.ReadLine());    
+            Console.WriteLine("Pole koła o promieniu {0} wynosi {1}",r, Math.PI*r*r);       
+            Console.WriteLine("Obwód koła o promieniu {0} wynosi {1}", r, 2*Math.PI*r);   
+            Console.ReadKey(true);   
 
         }
 
