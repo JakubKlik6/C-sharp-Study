0353843de56da330bfdce3f37f3c0a17173fe9ce
Jakub Klik <Jakub.klik6@gmail.com>
2022-02-22 10:40:49 +0100

    Update Zad3.cs

--- Lab2/Zad3.cs
+++ Lab2/Zad3.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
             int a;
             Console.Write("Podaj liczbę arabską od 1 do 1 000 000: ");
             a = Convert.ToInt32(Console.ReadLine());
-            // setki tysięcy 
+
             if (a >= 900000)  { a -= 900000; Console.Write("dziewięćset tysięcy "); }
             if (a >= 800000) { a -= 800000; Console.Write("osiemset tysięcy "); }
             if (a >= 700000) { a -= 700000; Console.Write("siedemset tysięcy "); }
@@ -20,7 +20,7 @@ namespace ConsoleApp1
             if (a >= 300000) { a -= 300000; Console.Write("trzysta tysięcy "); }
             if (a >= 200000) { a -= 200000; Console.Write("dwieście tysięcy "); }
             if (a >= 100000) { a -= 100000; Console.Write("sto tysięcy "); }
-            // dziesiątki tysięcy 
+            
             if (a >= 90000) { a -= 90000; Console.Write("dziewięćdziesiąt tysięcy "); }
             if (a >= 80000) { a -= 80000; Console.Write("osiemdziesiąt tysięcy "); }
             if (a >= 70000) { a -= 70000; Console.Write("siedemdziesiąt tysięcy "); }
@@ -30,7 +30,7 @@ namespace ConsoleApp1
             if (a >= 30000) { a -= 30000; Console.Write("trzydzieści tysięcy "); }
             if (a >= 20000) { a -= 20000; Console.Write("dwadzieścia tysięcy "); }
             if (a >= 10000) { a -= 10000; Console.Write("dziesięć tysięcy "); }
-            // jedności tysięcy 
+            
             if (a >= 9000) { a -= 9000; Console.Write("dziewięć tysięcy "); }
             if (a >= 8000) { a -= 8000; Console.Write("osiem tysięcy "); }
             if (a >= 7000) { a -= 7000; Console.Write("siedem tysięcy "); }
@@ -40,7 +40,7 @@ namespace ConsoleApp1
             if (a >= 3000) { a -= 3000; Console.Write("trzy tysiące "); }
             if (a >= 2000) { a -= 2000; Console.Write("dwa tysiące "); }
             if (a >= 1000) { a -= 1000; Console.Write("tysiąc "); }
-            // setki
+            
             if (a >= 900) { a -= 900; Console.Write("dziewięćset "); }
             if (a >= 800) { a -= 800; Console.Write("osiemset "); }
             if (a >= 700) { a -= 700; Console.Write("siedemset "); }
@@ -50,7 +50,7 @@ namespace ConsoleApp1
             if (a >= 300) { a -= 300; Console.Write("trzysta "); }
             if (a >= 200) { a -= 200; Console.Write("dwieście "); }
             if (a >= 100) { a -= 100; Console.Write("sto "); }
-            // dziesiątki
+            
             if (a >= 90) { a -= 90; Console.Write("dziewięćdziesiąt "); }
             if (a >= 80) { a -= 80; Console.Write("osiemdziesiąt "); }
             if (a >= 70) { a -= 70; Console.Write("siedemdziesiąt "); }
@@ -60,7 +60,7 @@ namespace ConsoleApp1
             if (a >= 30) { a -= 30; Console.Write("trzydzieści "); }
             if (a >= 20) { a -= 20; Console.Write("dwadzieścia "); }
             if (a >= 10) { a -= 10; Console.Write("dziesięć "); }
-            // jedności
+            
             if (a >= 9) { a -= 9; Console.Write("dziewięć"); }
             if (a >= 8) { a -= 8; Console.Write("osiem"); }
             if (a >= 7) { a -= 7; Console.Write("siedem"); }    
@@ -72,7 +72,7 @@ namespace ConsoleApp1
             if (a >= 1) { a -= 1; Console.Write("jeden"); }
 
 
-            Console.ReadKey(true);  //pauza
+            Console.ReadKey(true); 
         }
 
 
