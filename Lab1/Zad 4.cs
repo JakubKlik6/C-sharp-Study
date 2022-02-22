d0eec643d08674cf433a0825cf4bd1bbe9d3a102
Jakub Klik <Jakub.klik6@gmail.com>
2022-02-22 10:38:11 +0100

    Update Zad 4.cs

--- Lab1/Zad 4.cs
+++ Lab1/Zad 4.cs
@@ -12,10 +12,10 @@ namespace ConsoleApp1
             Console.Write("Podaj długość boku b= ");
             b = Convert.ToDouble(Console.ReadLine());
             Console.Write("Podaj długość wysokośći H= ");
-            H = Convert.ToDouble(Console.ReadLine());       //prosimy użytkownika o podanie boków oraz wysokości
-            Console.WriteLine("Objętość prostopadłościanu wynosi {0}",a*b*H);       //obliczamy V i wypisujemy wynik
-            Console.WriteLine("Pole prostopadłościany wynosi {0}",(2*a*b)+(2*b*H)+(2*a*H));     //Obliczamy pole i wypisujemy wynik
-            Console.ReadKey(true);      //pauza
+            H = Convert.ToDouble(Console.ReadLine());      
+            Console.WriteLine("Objętość prostopadłościanu wynosi {0}",a*b*H);       
+            Console.WriteLine("Pole prostopadłościany wynosi {0}",(2*a*b)+(2*b*H)+(2*a*H));    
+            Console.ReadKey(true);      
 
         }
 
